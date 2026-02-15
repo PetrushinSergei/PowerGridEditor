7f767963fa498039108ad3a9d074d2eb1206058f
PetrushinSergei <128285095+PetrushinSergei@users.noreply.github.com>
2026-02-15 21:02:55 +0700

    Remove duplicate System.Collections.Generic using in NodeForm

--- PowerGridEditor 2/Forms/NodeForm.cs
+++ PowerGridEditor 2/Forms/NodeForm.cs
@@ -6,7 +6,6 @@ using System.Windows.Forms;
 using System.Globalization;
 using System.Threading.Tasks;
 using System.Net.Sockets;
-using System.Collections.Generic;
 using System.Linq;
 using NModbus;
 
